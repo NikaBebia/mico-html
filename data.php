52f6cb71ba41cafc9f5cfdcc1e7b9b07280de708
unknown <nikabebia@gmail.com>
2026-09-04 23:59:04 +0400

    Add content in data.php file

---
+++ data.php
@@ -0,0 +1,91 @@
+<?php
+
+// 1. ნავიგაცია / კატეგორიები
+$categories = [
+    [
+        'title' => 'Home',
+        'link'  => 'index.php'
+    ],
+    [
+        'title' => 'About',
+        'link'  => 'about.html'
+    ],
+    [
+        'title' => 'Treatment',
+        'link'  => 'treatment.html'
+    ],
+    [
+        'title' => 'Doctors',
+        'link'  => 'doctor.html'
+    ],
+    [
+        'title' => 'Testimonial',
+        'link'  => 'testimonial.html'
+    ],
+    [
+        'title' => 'Contact Us',
+        'link'  => 'contact.html'
+    ],
+];
+
+// 2. Hospital Treatment სექცია
+$treatments = [
+    [
+        'title' => 'Nephrologist Care',
+        'text'  => 'Alteration in some form, by injected humour, or randomised words.',
+        'image' => 'images/t1.png'
+    ],
+    [
+        'title' => 'Eye Care',
+        'text'  => 'Alteration in some form, by injected humour, or randomised words.',
+        'image' => 'images/t2.png'
+    ],
+    [
+        'title' => 'Pediatrician Clinic',
+        'text'  => 'Alteration in some form, by injected humour, or randomised words.',
+        'image' => 'images/t3.png'
+    ],
+    [
+        'title' => 'Parental Care',
+        'text'  => 'Alteration in some form, by injected humour, or randomised words.',
+        'image' => 'images/t4.png'
+    ],
+];
+
+// 3. Our Doctors სექცია
+$doctors = [
+    [
+        'name'   => 'Hennry',
+        'degree' => 'MBBS',
+        'image'  => 'images/team1.jpg'
+    ],
+    [
+        'name'   => 'Jenni',
+        'degree' => 'MBBS',
+        'image'  => 'images/team2.jpg'
+    ],
+    [
+        'name'   => 'Morco',
+        'degree' => 'MBBS',
+        'image'  => 'images/team3.jpg'
+    ],
+];
+
+// 4. Testimonials სექცია
+$testimonials = [
+    [
+        'name' => 'Morijorch',
+        'role' => 'Default model text',
+        'text' => 'Editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites.'
+    ],
+    [
+        'name' => 'Rochak',
+        'role' => 'Default model text',
+        'text' => 'Various editors now use Lorem Ipsum as their default model text.'
+    ],
+    [
+        'name' => 'Brad Johns',
+        'role' => 'Default model text',
+        'text' => 'A search for lorem ipsum will uncover many web sites still in their infancy.'
+    ]
+];
